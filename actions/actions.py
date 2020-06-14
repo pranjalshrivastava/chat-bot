587719eada2bcff717c11875f5e7b2a7f25e81c5
Darya Gahramanova <darya1@usf.edu>
2020-06-14 18:09:09 -0400

    slotset

--- actions/actions.py
+++ actions/actions.py
@@ -10,7 +10,7 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.events import UserUtteranceReverted, SlotSet
 import requests
 import json
 
