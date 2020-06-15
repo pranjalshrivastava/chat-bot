d763150efe5cebf75e95d78a622c784735aab005
Darya Gahramanova <darya1@usf.edu>
2020-06-15 07:53:31 -0400

    url

--- actions/actions.py
+++ actions/actions.py
@@ -13,6 +13,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted, SlotSet
 import requests
 import json
+import socket
 
 class ActionGreetUser(Action):
 
@@ -32,7 +33,9 @@ class GetPanasScore(Action):
         return "action_get_panas_score"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        PATH = "http://35.237.71.46/panas-score"
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name) 
+        PATH = "http://" + host_ip + "/panas-score"
         data = requests.get(url=PATH).json()
         if data:
             score = data["score"]
