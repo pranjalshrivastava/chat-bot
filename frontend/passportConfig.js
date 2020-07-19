247dd5c9195c64982dd6d48ba3bb5927b20f9385
Darya Gahramanova <darya1@usf.edu>
2020-07-19 13:42:55 -0400

    authentication

--- frontend/passportConfig.js
+++ frontend/passportConfig.js
@@ -1,6 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
 const { pool } = require("./dbConfig");
-const bcrypt = require("bcrypt");
 
 function initialize(passport) {
     console.log("Initialized");
