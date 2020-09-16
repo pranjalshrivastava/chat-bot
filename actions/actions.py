a10660d9906e1772557849e4f359608d5365f116
Surya Manthena <54606418+RohithManthena@users.noreply.github.com>
2020-09-16 10:54:22 -0500

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -7,7 +7,7 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List, Union
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted, SlotSet, EventType, ConversationPaused
