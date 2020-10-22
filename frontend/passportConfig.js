697b770b3ddfb6ee4f0a1e34876ede2ff140095a
Surya Manthena <54606418+RohithManthena@users.noreply.github.com>
2020-10-22 02:43:11 -0500

    Update passportConfig.js

--- frontend/passportConfig.js
+++ frontend/passportConfig.js
@@ -4,30 +4,34 @@ const { pool } = require("./dbConfig");
 function initialize(passport) {
     console.log("Initialized");
 
-    const authenticateUser = (firstname, uid, done) => {
+    const authenticateUser = (uid, pwd, done) => {
         console.log(uid);
-        let queryText = `SELECT * FROM users WHERE uid = $1`
-        pool.query(queryText, [uid], (err, results) => {
+        let queryText = `SELECT * FROM users WHERE uid = $1 and pwd = $2`
+        pool.query(queryText, [uid,pwd], (err, results) => {
             if (err) {
                 throw err;
             }
             console.log(results.rows);
 
             if (results.rows.length > 0) {
-                const user = results.rows[0];
-                let queryText = `UPDATE users SET times_logged = $1 WHERE uid = $2`
-                pool.query(queryText, [user.times_logged + 1, user.uid]);
-                return done(null, user);
+                
+
+               const user = results.rows[0];
+               let queryText = `UPDATE users SET times_logged = $1 WHERE uid = $2`
+               pool.query(queryText, [user.times_logged + 1, user.uid]);
+               return done(null, user);
+
+
             } else {
                 return done(null, false, {
-                    message: "No user with that U-number. Please register."
+                    message: "Username or Password is wrong, please try again"
                 });
             }
         });
     };
 
     passport.use(
-        new LocalStrategy({ usernameField: "uid", passwordField: "uid" },
+        new LocalStrategy({ usernameField: "uid", passwordField: "pwd" },
             authenticateUser
         )
     );
