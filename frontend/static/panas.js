06835525961058749206f6f0f49ef6195e9b39fa
Surya Manthena <54606418+RohithManthena@users.noreply.github.com>
2020-11-12 05:17:27 -0600

    Update panas.js
    removed the model block

--- frontend/static/panas.js
+++ frontend/static/panas.js
@@ -16,23 +16,23 @@ $(document).ready(function() {
     
         // model block 
     
-        var modal = document.getElementById("myModal");
+        //var modal = document.getElementById("myModal");
 
 
         // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("popper-close")[0];
+        //var span = document.getElementsByClassName("popper-close")[0];
     
         // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
+        //span.onclick = function() {
+          //modal.style.display = "none";
+        //}
 
         // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          } 
-        }
+        //window.onclick = function(event) {
+         // if (event.target == modal) {
+          //  modal.style.display = "none";
+          //} 
+        //}
 
     //$("#bgStorySetupModal").modal()
     $("#panas_result").click(function() {
@@ -118,13 +118,13 @@ $(document).ready(function() {
                 dataType: 'json',
             });
             
-            setTimeout(function () {
-               window.open(window.location.href.replace("panas", 'chatbot'), '_blank');
-            }, 5000);
+            //setTimeout(function () {
+             //  window.open(window.location.href.replace("panas", 'chatbot'), '_blank');
+            //}, 5000);
                 
             //window.open(window.location.href.replace("panas", 'chatbot'), '_blank');
                 
-            //window.location.replace(window.location.href.replace("panas", 'chatbot'))
+            window.location.replace(window.location.href.replace("panas", 'cwb'))
         }
     });
 
