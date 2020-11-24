31b3dba89b225b1e0ce15e6dcb804e08f7dc81f1
Surya Manthena <54606418+RohithManthena@users.noreply.github.com>
2020-11-24 22:43:04 +0530

    Update cwb.js
    removed model block completely

--- frontend/static/cwb.js
+++ frontend/static/cwb.js
@@ -14,25 +14,6 @@ $(document).ready(function() {
     }
     values.forEach(removeErrors);
     
-        // model block 
-    
-        var modal = document.getElementById("myModal");
-
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("popper-close")[0];
-    
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          } 
-        }
 
     //$("#bgStorySetupModal").modal()
     $("#panas_result").click(function() {
